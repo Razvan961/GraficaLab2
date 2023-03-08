fcb2a7090512709c60174ad82cc9d47a5ef92d61
razvandragomir50@gmail.com <razvandragomir50@gmail.com>
2023-03-08 10:08:30 +0000

    Aplicatia 1

--- Laboratorul2/patrat.cpp
+++ Laboratorul2/patrat.cpp
@@ -1,13 +1,16 @@
 ﻿/*
 Programul afiseaza un patrat pe care il translateaza pe axa x la apasarea sagetilor stanga, dreapta
 */
-#include "glos.h"
+#include "GLOS.h"
 
+#include<math.h>
 #include "gl.h"
 #include "glu.h"
 #include "glaux.h"
+#include "glui.h"
 
 static GLfloat x = 0;
+static GLfloat y = 0;
 
 void myInit() {
     glClearColor(1.0, 1.0, 1.0, 1.0);
@@ -23,6 +26,15 @@ void CALLBACK MutaDreapta()
 {
     x = x + 5;
 }
+void CALLBACK MutaSus()
+{
+    y = y + 5;
+}
+void CALLBACK MutaJos()
+{
+    y = y - 5;
+}
+
 
 void CALLBACK display()
 {
@@ -30,10 +42,11 @@ void CALLBACK display()
 
     glLoadIdentity();
 
-    glTranslatef(x, 0, 0.0);
+    glTranslatef(x, y, 0.0);
 
-    glBegin(GL_QUADS);
+    glBegin(GL_POLYGON);
     {
+
         glColor3f(1.0, 0.0, 0.0);
         glVertex2f(100, 100);
         glColor3f(1.0, 1.0, 0.0);
@@ -42,7 +55,16 @@ void CALLBACK display()
         glVertex2f(150.0, 150.0);
         glColor3f(0.0, 1.0, 0.0);
         glVertex2f(100.0, 150.0);
+        for (int i = 0; i < 8; i++)
+        {
+            glVertex3f(1.0, 0.0, 0.0);
+                glVertex2f(cos((3.1415/4) * i) * r;
+        }
+       
     }
+
+
+    
     glEnd();
 
     glFlush();
@@ -78,7 +100,7 @@ void CALLBACK myReshape(GLsizei w, GLsizei h)
 }
 #endif
 
-int main(int argc, char** argv)
+int main(int argc, char** argv,int i,int r=1)
 {
     auxInitDisplayMode(AUX_SINGLE | AUX_RGB);
     auxInitPosition(0, 400, 800, 600);
@@ -86,7 +108,8 @@ int main(int argc, char** argv)
     myInit();
     auxKeyFunc(AUX_LEFT, MutaStanga);
     auxKeyFunc(AUX_RIGHT, MutaDreapta);
- 
+    auxKeyFunc(AUX_UP, MutaSus);
+    auxKeyFunc(AUX_DOWN, MutaJos);
 
     auxReshapeFunc(myReshape);
     auxMainLoop(display);
